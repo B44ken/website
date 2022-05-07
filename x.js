5eb09ecb981da729596511ceee442581d18de00e
brad <16675719+B44ken@users.noreply.github.com>
2022-05-07 16:04:09 -0400

    dated and unused

--- x.js
+++
@@ -1,8 +0,0 @@
-// redirect if query matches - like p27.ca?hemingway
-
-const links = {
-	"hemingway": () => "https://vault.fbi.gov/ernest-miller-hemingway/ernest-hemingway-part-01-of-01/at_download/file"
-}
-
-var func = document.location.search.replace('?','')
-if(links[func]) document.location.href = links[func]()
