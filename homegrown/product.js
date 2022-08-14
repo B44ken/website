6773a896114785e504900d3b53313e93f8c42599
b44ken <bradboratto@gmail.com>
2022-08-14 06:27:45 +0000

    giant backlog of changes

---
+++ homegrown/product.js
@@ -0,0 +1,6 @@
+const products = {
+    "native-harvest": {
+        "producer": "Native Harvest is made of Ojibwe tribe members and is based in Callaway, Minnesota. Their mission is to preserve the community's local seeds and foods.",
+        "sourcing": "Native Harvest runs their own farm, where all of their food products are handmade. Their clothing is made by a supplier."
+    }
+}
