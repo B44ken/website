badf86180b833f1bd9a8623d223def78e7eabb03
b44ken <bradboratto@gmail.com>
2022-08-25 00:35:28 +0000

    sync after a fat while

---
+++ roundcube/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Local configuration for Roundcube Webmail */
+
+$config['smtp_port'] = 25;
+$config['smtp_user'] = '';
+$config['db_dsnw'] = 'mysql://roundcube:Buttsex420@localhost/roundcubemail';
+$config['default_host'] = 'p27.ca';
+$config['support_url'] = '';
+$config['des_key'] = 'iR9IFBBWG0nYMrXSbnRyjXuD';
+$config['product_name'] = 'roundcube';
+$config['plugins'] = array();
